1e33826012f797ea2d2866965469458bd7cb04ee
lzc <3147025601@qq.com>
2022-11-21 16:54:34 +0800

    12.3answer

--- main.cpp
+++ main.cpp
@@ -1,53 +1,31 @@
 #include <iostream>
 using namespace std;
-#include "string2.h"
+#include "stock20.h"
 
-
+const int STKS = 4;
 int main()
 {
-    using std::cout;
-    using std::endl;
-    String s1("and I am a C++ student.");
-    String s2 = "Please enter your name:";
-    String s3;
-    cout << s2;
-    cin >> s3;
-    s2 = "My name is " + s3;
-    cout << s2 << ".\n";
-    s2 = s2 + s1;
-    s2.stringup();
-    cout << "The string \n";
-    cout << s2 << "\ncontains " << s2.has('A');
-    cout << " 'A' characters in it.\n";
-    s1 = "red";
 
-    String rgb[3] = {String(s1), String("green"), String("blue")};
-    cout << "Enter the name of a primary color for mixing light: ";
-    String ans;
-    bool success = false;
-    while (cin >> ans)
+    Stock stocks[STKS] = 
     {
-        ans.stringlow();
-        for (int i = 0; i < 3; i++)
-        {
-            if (ans == rgb[i])
-            {
-                cout << "That's right!\n";
-                success = true;
-                break;
-            }
-        }
-        if (success)
-        {
-            break;
-        }
-        else
-        {
-            cout << "Try again!\n";
-        }
-    }
-    cout << "Bye\n";
+        Stock("NanoSmart", 12, 20.0),
+        Stock("Boffo Objects", 200, 2.0),
+        Stock("Monolithic Obelisks", 130, 3.25),
+        Stock("Fleep Enterprises", 60, 6.5)
+    };
 
+    std::cout << "Stock holdings:\n";
+    for (int st = 0; st < STKS; st++)
+    {
+        std::cout << stocks[st] << std::endl;
+    }
+    const Stock *top = &stocks[0];
+    for (int st = 1; st < STKS; st++)
+    {
+        top = &top->topval(stocks[st]);
+    }
+    std::cout << "\nMost valuable holding:\n";
+    std::cout << *top;
     return 0;
 
 
---
+++ stock20.h
@@ -0,0 +1,26 @@
+#ifndef STOCK20_H_
+#define STOCK20_H_
+#include <iostream>
+using std::ostream;
+using std::istream;
+
+class Stock{
+    private:
+        char* company;
+        int shares;
+        double share_val;
+        double total_val;
+        void set_tot() {total_val = shares * share_val;}
+    public:
+        Stock();
+        Stock(const std::string &co, long n = 0, double pr = 0.0);
+        ~Stock();
+        void buy(long num, double price);
+        void sell(long num, double price);
+        void update(double price);
+        const Stock & topval(const Stock &s) const;
+        friend ostream & operator<<(ostream &os, const Stock &s);
+
+};
+
+#endif
